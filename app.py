c6473d5a6d96eb2dcaeef4c8d4cc2b459a9b733b
mcapxx <chetanmahesh1620@gmail.com>
2024-01-27 22:44:25 +0530

    last commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request
+from werkzeug import Markup
 import numpy as np
 import pandas as pd
 from utils.fertilizer import fertilizer_dic
